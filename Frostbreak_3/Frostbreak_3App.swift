410a1df6f855c9a0c4525c44859846837dc6d55f
Batxzy <jose.lopez4636@alumnos.udg.mx>
2025-04-03 07:23:09 -0600

    Initial Commit

---
+++ Frostbreak_3/Frostbreak_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Frostbreak_3App.swift
+//  Frostbreak_3
+//
+//  Created by Jose julian Lopez on 03/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Frostbreak_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
